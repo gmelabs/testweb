bb06364ba0355072a4bc09d5cacd051d4f69a614
alain <alainmungia@hotmail.com>
2014-04-22 16:47:42 +0200

    Deshacer después este cambio. Metida dependencia "rara"

--- testweb/src/main/java/acn/b/gmelabs/web/Prueba.java
+++ testweb/src/main/java/acn/b/gmelabs/web/Prueba.java
@@ -1,7 +1,12 @@
 package acn.b.gmelabs.web;
 
+import com.squareup.pollexor.Thumbor;
+
 public class Prueba {
 	public int suma(int num1, int num2) {
 		return num1 + num2;
+	}
+	public Thumbor getThumbor() {
+		return Thumbor.create("localhost");
 	}
 }
