cf7978785978aaead8b8166e13b729295c63e690
alain <alainmungia@hotmail.com>
2014-04-22 17:56:13 +0200

    Deshecho cambio anterior

--- testweb/src/main/java/acn/b/gmelabs/web/Prueba.java
+++ testweb/src/main/java/acn/b/gmelabs/web/Prueba.java
@@ -1,12 +1,7 @@
 package acn.b.gmelabs.web;
 
-import com.enioka.jqm.api.JobManager;
-
 public class Prueba {
 	public int suma(int num1, int num2) {
 		return num1 + num2;
-	}
-	public void m() {
-		JobManager jm = null;
 	}
 }
